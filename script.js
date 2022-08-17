ead59fbb5312a3e87f7649b7c28e894bb08b8f30
Kolawole Kayode <mightybalogun@gmail.com>
2022-08-17 08:08:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
